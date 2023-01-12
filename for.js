729bfb9e7755fb771a98c99a13b3836c97185747
Tiala Young <tialayoung@gmail.com>
2023-01-11 19:47:57 -0700

    Complete for statement practice

--- for.js
+++ for.js
@@ -3,17 +3,29 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
-
+for (var i = 25; i < 75; i ++ ){
+  if ( i % 2 === 0) {
+    console.log(i)
+}}
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
 
+for (var i = 0; i < weather_options.length; i ++ ){
+    console.log("Today's weather is " + weather_options[i] + ".");
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+
+for (var i = 0; i < words.length; i ++ ){
+  if (words[i].length === 3){
+    console.log(words[i]);
+  }
+}
+
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
